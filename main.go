2ae390dcfdccdab87a6f00bcc564193d90b99f77
zzhirong <zzhirong@gmail.com>
2025-04-23 00:05:46 +0800

    feat: 使用 flag 包解析命令行参数并添指定显示个数
    引入 flag 包以解析命令行参数，允许用户指定网络接口和显示前 n 个流量最高的 IP 地址。同时，添加排序功能以按流量降序显示结果，并限制显示数量以提高可读性。

--- main.go
+++ main.go
@@ -2,12 +2,14 @@ package main
 
 import (
 	"encoding/binary"
+	"flag"
 	"html/template"
 	"log"
 	"net"
 	"net/http"
 	"os"
 	"os/signal"
+	"sort"
 	"syscall"
 
 	"github.com/cilium/ebpf/link"
@@ -20,51 +22,7 @@ type IPStats struct {
 }
 
 //go:generate go run github.com/cilium/ebpf/cmd/bpf2go -cc clang traffic traffic.c -- -I/usr/include/x86_64-linux-gnu
-
-func main() {
-	if len(os.Args) < 3 || os.Args[1] != "-iface" {
-		log.Fatal("请指定网络接口，例如: -iface eth0")
-	}
-
-	ifaceName := os.Args[2]
-	iface, err := netlink.LinkByName(ifaceName)
-	if err != nil {
-		log.Fatalf("获取网络接口失败: %v", err)
-	}
-
-	objs := trafficObjects{}
-	if err := loadTrafficObjects(&objs, nil); err != nil {
-		log.Fatalf("加载 eBPF 对象失败: %v", err)
-	}
-	defer objs.Close()
-
-	prog := objs.TrafficMonitor
-	xdpLink, err := link.AttachXDP(link.XDPOptions{
-		Program:   prog,
-		Interface: iface.Attrs().Index,
-		Flags:     link.XDPGenericMode, // 使用通用模式以提高兼容性
-	})
-	if err != nil {
-		log.Fatalf("附加 XDP 程序失败: %v", err)
-	}
-	defer xdpLink.Close()
-
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		var stats []IPStats
-		var key uint32
-		var value uint64
-
-		iter := objs.IpStats.Iterate()
-		for iter.Next(&key, &value) {
-			ip := make(net.IP, 4)
-			binary.LittleEndian.PutUint32(ip, key)
-			stats = append(stats, IPStats{
-				IP:    ip.String(),
-				Bytes: value,
-			})
-		}
-
-		tmpl := `
+var tmpl = `
 		<!DOCTYPE html>
 		<html>
 		<head>
@@ -107,7 +65,65 @@ func main() {
 		</html>
 		`
 
-		t := template.Must(template.New("stats").Parse(tmpl))
+func main() {
+	var ifaceName string
+	var limit int
+	flag.StringVar(&ifaceName, "iface", "eth0", "网络接口名称，例如: eth0")
+	flag.IntVar(&limit, "n", 30, "显示前n个流量最高的IP地址")
+	flag.Parse()
+
+	if ifaceName == "" {
+		log.Fatal("请指定网络接口，例如: -iface eth0 -n 30")
+	}
+
+	iface, err := netlink.LinkByName(ifaceName)
+	if err != nil {
+		log.Fatalf("获取网络接口失败: %v", err)
+	}
+
+	objs := trafficObjects{}
+	if err = loadTrafficObjects(&objs, nil); err != nil {
+		log.Fatalf("加载 eBPF 对象失败: %v", err)
+	}
+	defer objs.Close()
+
+	prog := objs.TrafficMonitor
+	xdpLink, err := link.AttachXDP(link.XDPOptions{
+		Program:   prog,
+		Interface: iface.Attrs().Index,
+		Flags:     link.XDPGenericMode, // 使用通用模式以提高兼容性
+	})
+	if err != nil {
+		log.Fatalf("附加 XDP 程序失败: %v", err)
+	}
+	defer xdpLink.Close()
+
+	t := template.Must(template.New("stats").Parse(tmpl))
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		var stats []IPStats
+		var key uint32
+		var value uint64
+
+		iter := objs.IpStats.Iterate()
+		for iter.Next(&key, &value) {
+			ip := make(net.IP, 4)
+			binary.LittleEndian.PutUint32(ip, key)
+			stats = append(stats, IPStats{
+				IP:    ip.String(),
+				Bytes: value,
+			})
+		}
+
+		// 按Bytes降序排序
+		sort.Slice(stats, func(i, j int) bool {
+			return stats[i].Bytes > stats[j].Bytes
+		})
+
+		// 只保留前limit个结果
+		if len(stats) > limit {
+			stats = stats[:limit]
+		}
+
 		t.Execute(w, stats)
 	})
 
